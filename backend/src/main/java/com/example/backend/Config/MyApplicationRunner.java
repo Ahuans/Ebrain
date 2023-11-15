59b419f79733fa7d93cfc02bd219bb3a0b95f557
ZhibinChen <1179331904@qq.com>
2023-11-15 16:09:21 +0800

    Permission control, basic implementation of front-end and back-end interface writing

---
+++ backend/src/main/java/com/example/backend/Config/MyApplicationRunner.java
@@ -0,0 +1,19 @@
+package com.example.backend.Config;
+
+/**
+ * @author BruceChen
+ * @date 2023/11/01 15:41
+ **/
+import com.netty_server.factory.ZookeeperFactory;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyApplicationRunner implements ApplicationRunner {
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        ZookeeperFactory.initialCreate("localhost:2181");
+    }
+}
