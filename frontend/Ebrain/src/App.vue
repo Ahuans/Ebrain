13cbfbb3386a35d111e9203fee3a072ead434d1a
Lydia <2954665643@qq.com>
2023-11-19 15:06:31 +0800

    Change the logic of router with loginpage

--- frontend/Ebrain/src/App.vue
+++ frontend/Ebrain/src/App.vue
@@ -1,28 +1,70 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+const isLoggedIn = ref(false)
+// for test
+// const isLoggedIn = ref(true)
+// let username = ''
+// let password = ''
+
+const submitForm = async () => {
+  const formData = new FormData()
+  formData.append('username', username)
+  formData.append('password', password)
+
+  try {
+    const response = await fetch('http://localhost:8081/login', {
+      method: 'POST',
+      body: formData
+    })
+
+    if (response.ok) {
+      isLoggedIn.value = true
+    } else {
+      console.error('Fail. Please check your entries')
+    }
+  } catch (error) {
+    console.error('Error:', error)
+  }
+}
 </script>
 
 <template>
-  <header>
-    <div class="common-layout">
-      <el-container>
-        <el-aside width="200px">
-          <h1>Ebrain</h1>
-          <el-menu
-            default-active="2"
-            class="el-menu-vertical-demo"
-            
-          >
-            <el-menu-item index="1"  @click="$router.push('/')">ZooKeeper</el-menu-item>
-            <el-menu-item index="2" @click="$router.push('/ApiTester')">API</el-menu-item>
-          </el-menu>
-        </el-aside>
-        <el-main>
-          <RouterView />
-        </el-main>
-      </el-container>
-    </div>
-  </header>
+  <div>
+    <template v-if="!isLoggedIn">
+      <header>
+        <div class="common-layout">
+          <h1>Login Page</h1>
+          <form @submit.prevent="submitForm">
+            <label for="username">Username:</label>
+            <input type="text" id="username" v-model="username" required>
+            <br>
+            <label for="password">Password:</label>
+            <input type="password" id="password" v-model="password" required>
+            <br>
+            <button type="submit">Login</button>
+          </form>
+        </div>
+      </header>
+    </template>
+    <template v-else>
+      <header>
+        <div class="common-layout">
+          <el-container>
+            <el-aside width="200px">
+              <h1>Ebrain</h1>
+              <el-menu default-active="2" class="el-menu-vertical-demo">
+                <el-menu-item index="1" @click="$router.push('/admin')">ZooKeeper</el-menu-item>
+                <el-menu-item index="2" @click="$router.push('/ApiTester')">API</el-menu-item>
+              </el-menu>
+            </el-aside>
+            <el-main>
+              <RouterView />
+            </el-main>
+          </el-container>
+        </div>
+      </header>
+    </template>
+  </div>
 </template>
 
 <style scoped></style>
--- frontend/Ebrain/src/router/index.js
+++ frontend/Ebrain/src/router/index.js
@@ -3,8 +3,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+      {
+    path: '/',
+    name: 'login',
+    component: ()=>import("../views/LoginPage.vue")
+    },
     {
-      path: '/',
+      path: '/admin',
       name: 'zooKeeperView',
       component: ()=>import("../views/ZooKeeperView.vue")
     },
